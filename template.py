aa55144d292dddba008878be847343bc222da7a6
Malika <alinurbe@gmail.com>
2018-02-21 17:17:50 -0500

    stratified sampling

--- template.py
+++ template.py
@@ -1,11 +1,23 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.utils import to_categorical
+from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 
 # Preprocessing the Data
 x = np.reshape(np.load('images.npy'), (6500, 784))
 y = to_categorical(np.load('labels.npy'))
+
+# Stratified Sampling
+testSetSampler = StratifiedShuffleSplit(n_splits=10, test_size=0.25, train_size=0.75)
+for x_index, test_index in testSetSampler.split(x, y):
+    x_, x_test = x[x_index], x[test_index]
+    y_, y_test = y[x_index], y[test_index]
+
+validateSetSampler = StratifiedShuffleSplit(n_splits=10, test_size=0.2, train_size=0.8)
+for train_index, validate_index in testSetSampler.split(x_, y_):
+    x_train, x_val = x[train_index], x[validate_index]
+    y_train, y_val = y[train_index], y[validate_index]
 
 # Model Template
 
