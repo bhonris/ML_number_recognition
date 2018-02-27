f5aa5534f95f9af6fcafd9ce24a497603b620b5e
Bhon Bunnag <bhonris@gmail.com>
2018-02-26 21:46:02 -0500

    no change i cant change i cant change i cant change but im here in my mold i am here in my mold but im a million different people from one day to the next i cant change my mold no no no no noooooo

--- confusion_matrix_graph.py
+++ confusion_matrix_graph.py
@@ -2,35 +2,22 @@ import matplotlib.pyplot as plt
 import itertools
 import numpy as np
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
+def plot_confusion_matrix(cMat, classes,
+                          title='Confusion matrix'):
 
-    print(cm)
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.imshow(cMat, interpolation='nearest', cmap=plt.cm.Blues)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
+    fmt = 'd'
+    thresh = cMat.max() / 2.
+    for i, j in itertools.product(range(cMat.shape[0]), range(cMat.shape[1])):
+        plt.text(j, i, format(cMat[i, j], fmt),
                  horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+                 color="white" if cMat[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
